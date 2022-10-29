56e5b49d06f6ca866ed94ffd104ec3d4d058c0a2
Ali Özdemir <48456904+kabus4114@users.noreply.github.com>
2022-10-30 02:16:54 +0300

    Add files via upload

---
+++ vucut_kitle_indeksi_hesaplama/vucut_kitle_indeksi_hesaplama/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vucut_kitle_indeksi_hesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen Boyunuzu (Metre Cinsinden) giriniz : ");
+            double boy=Convert.ToDouble(Console.ReadLine());
+            Console.Write("Lütfen kilonuzu giriniz : ");
+            int kilo = Convert.ToInt32(Console.ReadLine());
+            double indeks = kilo / (boy * boy);
+            Console.Write("Vücut Kitle indeksiniz : "+indeks);
+            Console.ReadKey();
+        }
+    }
+}
