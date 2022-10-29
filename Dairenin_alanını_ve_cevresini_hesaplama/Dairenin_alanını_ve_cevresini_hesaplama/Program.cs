4d266b1e9b0839e6f3d1814ac5b03b4dbc25ba83
Ali Özdemir <48456904+kabus4114@users.noreply.github.com>
2022-10-30 02:11:41 +0300

    Add files via upload

---
+++ Dairenin_alanını_ve_cevresini_hesaplama/Dairenin_alanını_ve_cevresini_hesaplama/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dairenin_alanını_ve_cevresini_hesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14;
+            Console.Write("Dairenin Yarı Çapı : ");
+            int r = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Dairenin Merkez Ölçüsü : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            double alan = (pi * (r * r) * a) / 360;
+            Console.Write("Dairenin alanı : " + alan);
+            Console.ReadKey();
+        }
+    }
+}
