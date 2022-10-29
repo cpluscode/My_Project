34d51a71ee3a5eed8652455a4eb32ce8707c0a4e
Ali Özdemir <48456904+kabus4114@users.noreply.github.com>
2022-10-30 01:23:45 +0300

    Add files via upload

---
+++ kdv_hesaplama/kdv_hesaplama/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kdv_hesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {         
+            Console.Write("Hesaplanıcak tutar : ");
+            float sayi1 = Convert.ToSingle(Console.ReadLine());
+            if(sayi1 > 1000)
+            {
+                sayi1 = (sayi1) + sayi1 * 8 / 100;
+                Console.WriteLine("KDV Tutarı : "+sayi1);
+            }
+            else
+            {
+                sayi1 = (sayi1) + sayi1 * 18 / 100;
+                Console.WriteLine("KDV Tutarı : " + sayi1);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
