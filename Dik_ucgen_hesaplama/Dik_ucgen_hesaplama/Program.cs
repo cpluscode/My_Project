84faf1ab9f6d789a73e251331e1aa73697edc679
Ali Özdemir <48456904+kabus4114@users.noreply.github.com>
2022-10-30 01:52:45 +0300

    Add files via upload

---
+++ Dik_ucgen_hesaplama/Dik_ucgen_hesaplama/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dik_ucgen_hesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Üçgenin A Kenar Uzunluğunu Giriniz : ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Üçgenin B Kenar Uzunluğunu Giriniz : ");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Üçgenin C Kenar Uzunluğunu Giriniz : ");
+            int C = Convert.ToInt32(Console.ReadLine());
+            int d = (A + B + C) / 2;
+            double alan = Math.Sqrt(d * (d - A) * (d - B)*(d - C));
+            Console.Write("Üçgenin Alanı : " + alan);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Dik_ucgen_hesaplama/Dik_ucgen_hesaplama/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Dik_ucgen_hesaplama")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dik_ucgen_hesaplama")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("c1d7b919-8bad-4ffa-8734-ae9be2a669fe")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
