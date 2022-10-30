23506958295a7c4377cbdd4077cc033cdbfe7f62
Ali Özdemir <48456904+cpluscode@users.noreply.github.com>
2022-10-31 01:09:50 +0300

    Add files via upload

---
+++ etkinlik_önerme/etkinlik_önerme/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace etkinlik_önerme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sıcalık Değerini Giriniz : ");
+            int hava = Convert.ToInt32(Console.ReadLine());
+            if (hava < 5)
+                Console.Write("Bu havada kayak yapabilirsiniz");
+            else if (hava < 15 && hava > 5)
+                Console.Write("Bu havada Sinama izleyebilirsiniz");
+            else if (hava < 25 && hava > 15)
+                Console.Write("Bu havada Piknik yapabilirsiniz");
+            else if (hava >= 25)
+                Console.Write("Bu havada Yüzme yapabilirsiniz");
+            else
+                Console.Write("yapıcak etkinlik yok!");
+            Console.ReadKey();
+        }
+    }
+}
