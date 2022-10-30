3c17d9bff1dd2b8d9ff5a3283e11c77ec1c6f992
Ali Özdemir <48456904+cpluscode@users.noreply.github.com>
2022-10-31 01:17:53 +0300

    Add files via upload

---
+++ sayilari_siralama/sayilari_siralama/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sayilari_siralama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Bir sayı giriniz : ");
+                sayilar[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Array.Sort(sayilar);
+            foreach (int i in sayilar)
+            {
+                Console.WriteLine("{0}", i);
+            }
+            Console.Write("Sıralama Yapıldı!");
+            Console.ReadKey();
+        }
+    }
+}
