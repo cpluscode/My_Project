eb0f5028a7d73d35fbe7d194578a7e63154b2572
Ali Özdemir <48456904+cpluscode@users.noreply.github.com>
2022-10-31 13:51:20 +0300

    Add files via upload

---
+++ Artik_yil/Artik_yil/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artik_yil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Yıl Giriniz : ");
+            int yil = Convert.ToInt32(Console.ReadLine());
+            int islem = yil % 4;
+            switch (islem)
+            {
+                case 0:
+                    Console.Write("Bu yıl artık yıldır");
+                    break;
+                case 1:
+                    Console.Write("Bu yıl bir Artık Yıl değildir");
+                    break;
+                case 2:
+                    Console.Write("Bu yıl bir Artık Yıl değildir.");
+                    break;
+                case 3:
+                    Console.Write("Bu yıl bir Artık Yıl değildir.");
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
