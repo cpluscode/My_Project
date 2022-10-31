65b6d6a49e2db1118465ea8340a2744f7753132a
Ali Özdemir <48456904+cpluscode@users.noreply.github.com>
2022-10-31 14:39:27 +0300

    Add files via upload

---
+++ tek_sayida_çift_4katları_toplama/tek_sayida_çift_4katları_toplama/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tek_sayida_çift_4katları_toplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi,toplam = 0;
+            do 
+            {
+                Console.Write("Sayı giriniz : ");
+                sayi = Convert.ToInt32(Console.ReadLine());
+                if ((sayi % 2) == 0)
+                {
+                    toplam += sayi;
+                }
+            } 
+            while (sayi>0);
+            Console.Write("Çift Sayıların toplam değeri : "+toplam);
+            Console.ReadKey();
+        }
+    }
+}
