4be5e3b96dbcc8ee9896d2ec4a1ce206bae57d94
Ali Özdemir <48456904+cpluscode@users.noreply.github.com>
2022-10-31 14:27:02 +0300

    Add files via upload

---
+++ sayi_kadar_cift_bulma/sayi_kadar_cift_bulma/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sayi_kadar_cift_bulma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 0 , toplam=0;
+            Console.Write("Sayı Giriniz : ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= sayi; i++)
+            {
+                if ((i % 4) == 0 && (i % 3) == 0)
+                {
+                    toplam += i;
+                    sayac++;
+                }
+            }
+            int ortalama = toplam/sayac;
+            Console.Write("Toplam Sayı : "+toplam);
+            Console.Write("Ortalama Sayı : "+ortalama);
+            Console.ReadKey();
+        }
+    }
+
+}
